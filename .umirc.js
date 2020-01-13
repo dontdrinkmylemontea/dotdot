c344f3e81d5c8eea27c3d30b96639b57a2e2b530
dontdrinkmylemmontea <492861128@qq.com>
2020-01-13 15:28:38 +0800

    修改路由方式

--- .umirc.js
+++ .umirc.js
@@ -1,30 +1,33 @@
-
 // ref: https://umijs.org/config/
+const pathName = './';
+
 export default {
+  base: pathName,
+  publicPath: pathName,
+  history: 'hash',
   treeShaking: true,
   routes: [
     {
       path: '/',
       component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+      routes: [{ path: '/', component: '../pages/index' }],
+    },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: false,
-      dva: false,
-      dynamicImport: false,
-      title: 'dotdot',
-      dll: false,
-      
-      routes: {
-        exclude: [
-          /components\//,
-        ],
+    [
+      'umi-plugin-react',
+      {
+        antd: false,
+        dva: false,
+        dynamicImport: false,
+        title: 'dotdot',
+        dll: false,
+
+        routes: {
+          exclude: [/components\//],
+        },
       },
-    }],
+    ],
   ],
-}
+};
